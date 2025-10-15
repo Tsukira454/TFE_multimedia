dcb8f2923c67454f2f0454b4e0c1f137412a068e
tsukira <elvingamecraft@hotmail.com>
2025-10-15 19:20:00 +0200

    suit db

---
+++ www/secure/identifiant.php
@@ -0,0 +1,9 @@
+<?php 
+// C'est pour vous donc ne faite pas de la merde avec les mdp svp
+$DBHOST = "mysql-mouyart.alwaysdata.net";
+$DBNAME = "mouyart_rethro_pi";
+$DBUSER = "mouyart_rethropi";
+$DBPASSWORD = "Djhz462#fd8dmPGFa#_.??#";
+// C'est pour vous donc ne faites pas de la merde avec les mdp svp
+// (le mdp est un faux mdp donc pas le mien en privé)
+?>
